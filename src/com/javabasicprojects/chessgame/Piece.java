df81aea748d421d4ef0eb2ff3e3e8fb4376e7218
Andrii Synytsia <andreysinitsa4315@gmail.com>
2024-08-11 17:36:03 -0600

    Implemented abstract class Piece which represents the main behavior of chess pieces

--- src/com/javabasicprojects/chessgame/Piece.java
+++ src/com/javabasicprojects/chessgame/Piece.java
@@ -1,4 +1,25 @@
 package com.javabasicprojects.chessgame;
 
-public class Piece {
+public abstract class Piece {
+    protected Position position;
+    protected PieceColor color;
+
+    public Piece(Position position, PieceColor color) {
+        this.color = color;
+        this.position = position;
+    }
+
+    public PieceColor getPieceColor() {
+        return color;
+    }
+
+    public Position getPiecePosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public abstract boolean isValidMove(Position newPosition, Piece[][] board);
 }
