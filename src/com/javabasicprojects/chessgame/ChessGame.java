751f386eb20f29ff38a1c12e1266ad0cc707d6c4
Andrii Synytsia <andreysinitsa4315@gmail.com>
2024-08-11 20:15:18 -0600

    Initialized makeMove method for move handling

--- src/com/javabasicprojects/chessgame/ChessGame.java
+++ src/com/javabasicprojects/chessgame/ChessGame.java
@@ -7,4 +7,19 @@ public class ChessGame {
     public ChessGame() {
         this.board = new ChessBoard();
     }
+
+    public boolean makeMove(Position start, Position end) {
+        Piece movingPiece = board.getPiece(start.getRow(), start.getColumn());
+        if (movingPiece == null || movingPiece.getPieceColor() != (whiteTurn ? PieceColor.WHITE : PieceColor.BLACK)) {
+            return false; // No piece at the start position or not the player's turn
+        }
+
+        if (movingPiece.isValidMove(end, board.getBoard())) {
+            // Execute the move
+            board.movePiece(start, end);
+            whiteTurn = !whiteTurn; // Switch turns
+            return true;
+        }
+        return false;
+    }
 }
